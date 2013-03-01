69e8a7bfc57d43403e3e8136975fc70c550dbb41
abiyasa <ilham.suhardi@gmail.com>
2013-03-01 14:21:09 +0100

    include all core components

--- src/ash/ash-framework.js
+++ src/ash/ash-framework.js
@@ -3,12 +3,19 @@
  */
 define(function (require) {
     var core = {
-        version: '0.1.0'
+        VERSION: '0.1.0'
     };
 
     core.engine = require('ash-core/engine');
     core.componentmatchingfamily = require('ash-core/componentmatchingfamily');
     core.entity = require('ash-core/entity');
+    core.entitylist = require('ash-core/entitylist');
+    core.family = require('ash-core/family');
+    core.node = require('ash-core/node');
+    core.nodelist = require('ash-core/nodelist');
+    core.nodepool = require('ash-core/nodepool');
+    core.system = require('ash-core/system');
+    core.systemlist = require('ash-core/systemlist');
 
     return core;
 });
