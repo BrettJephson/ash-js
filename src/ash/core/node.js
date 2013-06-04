0593bbc491a0bf63ba3ad4e9aba244cb08fa5bd0
abiyasa <ilham.suhardi@gmail.com>
2013-06-04 15:32:57 +0200

    introduce a simpler way to create a Node

--- src/ash/core/node.js
+++ src/ash/core/node.js
@@ -10,9 +10,51 @@ define([
         entity: null,
         previous: null,
         next: null,
-        
+
         constructor: function () { }
     });
+
+    /**
+    * A simpler way to create a node.
+    *
+    * Example: creating a node for component classes Point &amp; energy:
+    *
+    * var PlayerNode = Ash.Node.create({
+    *   point: Point,
+    *   energy: Energy
+    * });
+    *
+    * This is the simpler version from:
+    *
+    * var PlayerNode = Ash.Node.extend({
+    *   point: null,
+    *   energy: null,
+    *
+    *   types: {
+    *     point: Point,
+    *     energy: Energy
+    *   }
+    * });
+    */
+    Node.create = function (schema) {
+        var processedSchema = {
+            types: {},
+            constructor: function () { }
+        };
+
+        // process schema
+        for (var propertyName in schema) {
+            if (schema.hasOwnProperty(propertyName)) {
+                var propertyType = schema[propertyName];
+                if (propertyType) {
+                    processedSchema.types[propertyName] = propertyType;
+                }
+                processedSchema[propertyName] = null;
+            }
+        }
+
+        return Node.extend(processedSchema);
+    };
 
     return Node;
 });
